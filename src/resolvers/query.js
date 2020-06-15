f626866af8323f66a904d89f6fcd33d1b087b84d
AmoDinho <>
2020-06-15 15:53:26 +0200

    added the function

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,7 +1,49 @@
+import dynamodb from "../../libs/dynamodb-lib";
 export const hello = (args, context) => {
   return "Your GraphQL API is now LIVE!🎈 ";
 };
 
 export const getAllListings = async (args, context) => {
-  return null;
+  const params = {
+    TableName: process.env.ListingsDB || "dev-listings",
+  };
+
+  try {
+    const result = await dynamoDBLib.call("scan", params);
+
+    if (result.Items.length === 0) {
+      return "You have no listings";
+    } else {
+      return result.Items.map((i) => ({
+        listingId: i.listingId,
+        coverPhoto: i.coverPhoto,
+        listingName: i.listingName,
+        listingDescription: i.listingDescription,
+        listingType: i.listingType.map((m) => ({
+          name: m,
+        })),
+        listingLocation: i.listingLocation,
+        listingActivities: i.listingActivities.map((k) => ({
+          name: k,
+        })),
+        specialType: i.specialType,
+        specialAmount: i.specialAmount,
+        rating: i.rating,
+        guide: {
+          Name: i.guide.name,
+          Bio: i.guide.bio,
+          Avatar: i.guide.avatar,
+        },
+        price: i.price,
+        numberOfDays: i.numberOfDays,
+      }));
+    }
+
+    // return result;
+  } catch (e) {
+    return {
+      message: e.message,
+      code: "500x",
+    };
+  }
 };
