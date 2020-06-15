2b8b7beb6ec83753c4ca61bc5abf50d6d9aa951f
AmoDinho <>
2020-06-15 16:21:29 +0200

    exported the mutations

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,9 +1,13 @@
 import { hello, getAllListings, getAListing } from "./query";
+import { makeABooking } from "./mutation";
 
 export const resolvers = {
   Query: {
     hello: (root, args, context) => hello(args, context),
     getAllListings: (root, args, context) => getAllListings(args, context),
     getAListing: (root, args, context) => getAListing(args, context),
+  },
+  Mutation: {
+    makeABooking: (root, args, context) => makeABooking(args, context),
   },
 };
--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,11 +1,3 @@
-/*
-
-ADD YOUR MUTATION HERE:
-
-export const addCars = async (args, context) => {
-    
-
-}
-
-
-*/
+export const makeABooking = async (args, context) => {
+  return null;
+};
