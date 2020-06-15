197f795c796dea385207f49c29dfbef0759024c5
AmoDinho <>
2020-06-15 16:11:47 +0200

    done with the function

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -49,5 +49,51 @@ export const getAllListings = async (args, context) => {
 };
 
 export const getAListing = async (args, context) => {
-  return null;
+  const params = {
+    TableName: process.env.ListingsDB || "dev-listings",
+    FilterExpression: "listingId = :listingId",
+    ExpressionAttributeValues: {
+      ":listingId": args.listingId,
+    },
+  };
+
+  try {
+    const listing = await dynamoDBLib.call("scan", params);
+
+    console.log(listing);
+
+    if (listing.Items.length === 0) {
+      return "There is no listing";
+    } else {
+      return {
+        listingName: listing.Items[0].listingName,
+
+        listingId: listing.Items[0].listingId,
+        coverPhoto: listing.Items[0].coverPhoto,
+        listingDescription: listing.Items[0].listingDescription,
+        listingType: listing.Items[0].listingType.map((m) => ({
+          name: m,
+        })),
+        listingLocation: listing.Items[0].listingLocation,
+        listingActivities: listing.Items[0].listingActivities.map((k) => ({
+          name: k,
+        })),
+        specialType: listing.Items[0].specialType,
+        specialAmount: listing.Items[0].specialAmount,
+        rating: listing.Items[0].rating,
+        guide: {
+          Name: listing.Items[0].guide.name,
+          Bio: listing.Items[0].guide.bio,
+          Avatar: listing.Items[0].guide.avatar,
+        },
+        price: listing.Items[0].price,
+        numberOfDays: listing.Items[0].numberOfDays,
+      };
+    }
+  } catch (e) {
+    return {
+      message: e.message,
+      code: "500x",
+    };
+  }
 };
