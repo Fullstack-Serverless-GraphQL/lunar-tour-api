c81cf2731be3d1170d423ef19499c865d54336d2
AmoDinho <>
2020-06-15 14:44:09 +0200

    can import the data succesfully

--- seed-data/insertData.js
+++ seed-data/insertData.js
@@ -3,6 +3,11 @@ AWS.config.update({ region: "us-east-1" });
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const listings = require("./listings");
-
+const listings = require("./listings.json");
 console.log("Listings.Init", listings);
+
+//lets inseert them into the table
+
+listings.map((l) => {
+  listingParams = {};
+});
