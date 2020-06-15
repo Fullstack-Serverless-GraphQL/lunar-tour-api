864daa2997398c9e92ce1d7e07ccfe45535de615
AmoDinho <>
2020-06-15 14:55:30 +0200

    added comments

--- seed-data/insertData.js
+++ seed-data/insertData.js
@@ -1,14 +1,21 @@
+/*
+
+this function will allow us to add all the listings into Dynamo dynamically
+*/
+
+//set up the aws-sdk
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-1" });
-
 const docClient = new AWS.DynamoDB.DocumentClient();
 
+//import the listings json
 const listings = require("./listings.json");
 console.log("Listings.Init", listings);
 
 //lets inseert them into the table
-
+//loop over the listings
 listings.map((l) => {
+  //create params object
   listingParams = {
     TableName: "dev-lunar-listings",
     Item: {
@@ -31,6 +38,8 @@ listings.map((l) => {
       specialType: l.specialType,
     },
   };
+
+  //put the data into the table
 
   docClient.put(listingParams, function (err, data) {
     if (err) {
