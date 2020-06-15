cd70cc25bd1dd8c78e29d1d798d3030c6947f501
AmoDinho <>
2020-06-15 15:50:37 +0200

    added listing query data

--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,36 @@
 const schema = `
 
-
+type ListingType {
+     name: String
+   }
+   
+   type ListingActivities {
+     name: String
+   }
+   
+   type Guide {
+     Name: String
+     Bio: String
+     Avatar: String
+   }
+   
+   type Listing {
+        listingId: String
+        coverPhoto: String
+        listingName: String
+        listingDescription: String
+        listingType: [ListingType]
+        listingLocation: String
+        listingActivities: [ListingActivities]
+        specialType: String
+        specialAmount: Int
+        rating: Int
+        guide: Guide
+        price: String
+        numberOfDays: Int
+     
+     }
+   What
 
 """
 A hello world Query
@@ -9,6 +39,6 @@ type Query {
      hello: String!
 }
 
-`
+`;
 
-export {schema}
+export { schema };
