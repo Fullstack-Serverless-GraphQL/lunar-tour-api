cd61eca7068de40a5e4c31da891f9eaf3207e769
AmoDinho <>
2020-06-15 14:47:38 +0200

    made the listing object

--- seed-data/insertData.js
+++ seed-data/insertData.js
@@ -9,5 +9,26 @@ console.log("Listings.Init", listings);
 //lets inseert them into the table
 
 listings.map((l) => {
-  listingParams = {};
+  listingParams = {
+    TableName: "dev-lunar-listings",
+    Item: {
+      coverPhoto: l.coverPhoto,
+      guide: {
+        avatar: l.guide.avatar,
+        bio: l.guide.bio,
+        name: l.guide.name,
+      },
+      listingActivities: l.listingActivities,
+      listingDescription: l.listingDescription,
+      listingId: l.listingId,
+      listingLocation: l.listingLocation,
+      listingName: l.listingName,
+      listingType: l.listingType,
+      numberOfDays: l.numberOfDays,
+      price: l.price,
+      rating: l.rating,
+      specialAmount: l.specialAmount,
+      specialType: l.specialType,
+    },
+  };
 });
