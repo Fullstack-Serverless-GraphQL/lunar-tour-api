ac52f29c971523f6e81d3364e129d235d3299e20
AmoDinho <u14284783@tuks.co.za>
2020-09-10 20:30:07 +0200

    added introspection

--- src/graphql.js
+++ src/graphql.js
@@ -1,35 +1,35 @@
-import {ApolloServer, gql} from "apollo-server-lambda";
-import {schema} from "./schema";
-import {resolvers} from "./resolvers";
-
+import { ApolloServer, gql } from "apollo-server-lambda";
+import { schema } from "./schema";
+import { resolvers } from "./resolvers";
 
 const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers: resolvers,
-    formatError: error => {
-		return error;
-	},
-	formatResponse: response => {
-		return response;
-	},
-	context:  ({event,context}) => ({
-		headers: event.headers,
-		functionName: context.functionName, 
-		event,
-	    context
-	}),
-	tracing: true,
-	playground: true
+  typeDefs: schema,
+  resolvers: resolvers,
+  formatError: (error) => {
+    return error;
+  },
+  formatResponse: (response) => {
+    return response;
+  },
+  context: ({ event, context }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+  }),
+  tracing: true,
+  playground: true,
+  introspection: true,
 });
 
 exports.graphqlHandler = (event, context, callback) => {
-    const handler = server.createHandler({
-        cors: {
-            origin: "*",
-            credentials:true,
-            methods:["POST","GET"],
-            allowedHeaders: ["Content-Type", "Origin", "Accept"]
-        }
-    });
-    return handler(event, context, callback);
+  const handler = server.createHandler({
+    cors: {
+      origin: "*",
+      credentials: true,
+      methods: ["POST", "GET"],
+      allowedHeaders: ["Content-Type", "Origin", "Accept"],
+    },
+  });
+  return handler(event, context, callback);
 };
